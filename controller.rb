9ac59473a1ac40443e931ee6f205ccb19821568f
Stephen Edward Ng <stephenedwardng@outlook.com>
2017-06-06 16:03:25 +0100

    Start point

---
+++ controller.rb
@@ -0,0 +1,46 @@
+require( 'sinatra' )
+require( 'sinatra/contrib/all' )
+require( 'pry-byebug' )
+
+require_relative './models/pizza_order'
+require_relative './models/customer'
+
+get '/pizzas' do
+  @pizzas = PizzaOrder.all()
+  erb(:index)
+end
+
+get '/pizza/new' do 
+  @customers = Customer.all()
+  erb(:new)
+end
+
+get '/pizza/:id' do
+  @pizza = PizzaOrder.find(params[:id])
+  erb(:show)
+end
+
+post '/pizzas' do
+  @pizza = PizzaOrder.new(params)
+  @pizza.save()
+  erb(:create)
+end
+
+get '/pizza/:id/edit' do 
+  @pizza = PizzaOrder.find(params[:id])
+  erb(:edit)
+end
+
+post '/pizza/:id' do
+  @pizza = PizzaOrder.find(params[:id])
+  @pizza.update(params)
+  redirect to "/pizza/#{params[:id]}"
+end
+
+post '/pizza/:id/delete' do
+  @pizza = PizzaOrder.find(params[:id])
+  @pizza.delete()
+  redirect to "/pizzas"
+end
+
+
